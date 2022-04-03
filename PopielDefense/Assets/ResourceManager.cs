2e04efb1d191e0bb8477fffdbaf55225023dbcae
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2022-04-03 22:45:32 +0200

    Created Resource Manager Again

---
+++ PopielDefense/Assets/ResourceManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    private uint money;
+
+    public void AddMoney(uint amount)
+    {
+        money += amount;
+    }
+
+    public void SubtractMonet(uint amount)
+    {
+        if (amount > money)
+        {
+            money = 0;
+        }
+        else
+        {
+            money -= amount;
+        }
+    }
+
+    public uint GetMoney()
+    {
+        return money;
+    }
+
+    public void SetMoney(uint amount)
+    {
+        money = amount;
+    }
+}
